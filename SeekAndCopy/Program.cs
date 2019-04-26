8e875ef4f9d75d313c64a5a46d52688b74f936d5
Tarik Eminagic <tarik.eminagic@cegeka.com>
2019-04-26 09:31:01 +0200

    Add project files.

---
+++ SeekAndCopy/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.IO;
+using System.Linq;
+
+namespace SeekAndCopy
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            var sourceFile = ConfigurationManager.AppSettings["file"];
+            var file = new StreamReader(sourceFile);
+
+            var listOfFiles = new List<string>();
+            var listOfFoundFiles = new List<string>();
+            string line;
+            while ((line = file.ReadLine()) != null)
+            {
+                listOfFiles.Add(line);
+            }
+
+            Console.WriteLine($"{sourceFile} loaded!");
+
+            var sourceDirectory = ConfigurationManager.AppSettings["source"];
+            var directoryInfo = new DirectoryInfo(sourceDirectory);
+            var destinationDirectory = ConfigurationManager.AppSettings["destination"];
+
+            Console.WriteLine("Copied files:");
+            CopyFiles(directoryInfo, listOfFiles, destinationDirectory, listOfFoundFiles);
+            Console.WriteLine("Not found files:");
+            foreach (var filename in listOfFiles.Except(listOfFoundFiles))
+            {
+                Console.WriteLine(filename);
+            }
+            Console.WriteLine("done");
+            Console.ReadLine();
+        }
+
+        private static void CopyFiles(DirectoryInfo directoryInfo, List<string> listOfFiles, string destinationDirectory, List<string> listOfFoundFiles)
+        {
+            foreach (var fileInfo in directoryInfo.EnumerateFiles())
+            {
+                if (listOfFiles.Contains(fileInfo.Name))
+                {
+                    var destination = $"{destinationDirectory}\\{fileInfo.Name}";
+                    File.Copy(fileInfo.FullName, destination, true);
+                    Console.WriteLine($"{fileInfo.FullName} -> {destination}");
+                    listOfFoundFiles.Add(fileInfo.Name);
+                }
+            }
+
+            foreach (var dirInfo in directoryInfo.EnumerateDirectories())
+            {
+                CopyFiles(dirInfo, listOfFiles, destinationDirectory, listOfFoundFiles);
+            }
+        }
+    }
+}
